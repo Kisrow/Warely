349f3ee1279d3186393cc28bdf7df3ce28827867
Kisrow <vlasekuznetsov@gmail.com>
2026-03-27 14:12:41 +0300

    init

---
+++ src/app/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import "antd/dist/reset.css";
+
+import { queryClient } from "@/shared/lib/queryClient";
+import App from "./App";
+
+import "@/index.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </StrictMode>,
+);
