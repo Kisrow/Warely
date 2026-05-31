64ceae40eef7662caec9b0ffa72e81c09397a1d2
Kisrow <vlasekuznetsov@gmail.com>
2026-05-31 10:48:10 +0300

    chore: добавил хаски, обновил зависимости

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -12,12 +12,7 @@ import "@/index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter
-        future={{
-          v7_startTransition: true,
-          v7_relativeSplatPath: true,
-        }}
-      >
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </QueryClientProvider>
